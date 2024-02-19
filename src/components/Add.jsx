b380ccde73777442726153f1e4aca6bf135d7d92
Naim Howlader <naimmd775@gmail.com>
2024-02-19 20:22:45 +0600

    update

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -10,7 +10,7 @@ export default function Add({addTodoHandler,currentTodo,setCurrentTodo}) {
     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
     <div className="relative">
         
-        <input  value={currentTodo} type="text" onChange={(e)=>setCurrentTodo(e.target.value)} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-0" placeholder="Write your todo list here..."  />
+        <input  value={currentTodo} type="text" onChange={(e)=>setCurrentTodo(e.target.value)} id="default-search" className="block w-full p-4 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-0" placeholder="Write your todo list here..."  />
         <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-gradient-to-tr from-sky-300 via-sky-400 to-blue-500  font-medium rounded-lg text-sm px-10 py-2 ">Add</button>
     </div>
 </form>
--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -10,7 +10,7 @@ export default function Edit({updateTodoHandler,currentTodo,setCurrentTodo,setIs
     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
     <div className="relative">
         
-        <input  value={currentTodo} type="text" onChange={(e)=>setCurrentTodo(e.target.value)} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-0"/>
+        <input  value={currentTodo} type="text" onChange={(e)=>setCurrentTodo(e.target.value)} id="default-search" className="block w-full p-4 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-0"/>
         <div className=''>
         <button type="submit" className="text-white absolute end-10 bottom-2.5 bg-gradient-to-tr from-sky-300 via-sky-400 to-blue-500  font-medium rounded-lg text-sm px-10 py-2 ">Update</button>
         <div style={{cursor:"pointer"}} onClick={()=>{setIsEditing(false);setCurrentTodo('')}} className='absolute end-2.5 bottom-3 text-2xl font-bold text-red-500'>
